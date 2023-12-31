62dbb4f47b6a4e2ba30ed9a9e5d9993d4883b856
usmanasif-dev <usman.asif@spycloud.com>
2023-12-31 04:24:19 -0800

    added background video and updated some css

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,41 +1,52 @@
-import React, { useState } from 'react';
-import Form from './Form';
+import React, { useState } from "react";
+import Form from "./Form";
+import video from "../background.mp4";
 
 const Auth = () => {
   const [isSignup, setIsSignup] = useState(false);
 
   return (
-    <div className={`data-container ${isSignup ? 'data-change' : ''}`}>
-      <div className="data-forms-container">
-        <Form type={isSignup ? 'signup' : 'signin'}/>
+    <>
+      <div className="fixed inset-0 z-[-1]">
+        <video className="w-full h-full object-cover" autoPlay muted loop>
+          <source src={video} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
       </div>
-      <div className="data-intros-container">
-        <div className="data-intro-control data-signin-intro">
-          <div className="data-intro-control__inner">
-            <h2>Welcome back!</h2>
-            <p>
-              Welcome back! We are so happy to have you here. It's great to see you again.
-              We hope you had a safe and enjoyable time away.
-            </p>
-            <button id="signup-btn" onClick={() => setIsSignup(true)}>
-              No account yet? Signup.
-            </button>
+
+      <div className={`data-container ${isSignup ? "data-change" : ""}`}>
+        <div className="data-forms-container">
+          <Form type={isSignup ? "signup" : "signin"} />
+        </div>
+        <div className="data-intros-container">
+          <div className="data-intro-control data-signin-intro">
+            <div className="data-intro-control__inner">
+              <h2>Welcome back!</h2>
+              <p>
+                Welcome back! We are so happy to have you here. It's great to
+                see you again. We hope you had a safe and enjoyable time away.
+              </p>
+              <button id="signup-btn" onClick={() => setIsSignup(true)}>
+                No account yet? Signup.
+              </button>
+            </div>
+          </div>
+          <div className="data-intro-control data-signup-intro">
+            <div className="data-intro-control__inner">
+              <h2>Come join us!</h2>
+              <p>
+                We are so excited to have you here. If you haven't already,
+                create an account to get access to exclusive offers, rewards,
+                and discounts.
+              </p>
+              <button id="signin-btn" onClick={() => setIsSignup(false)}>
+                Already have an account? Signin.
+              </button>
+            </div>
           </div>
         </div>
-        <div className="data-intro-control data-signup-intro">
-          <div className="data-intro-control__inner">
-            <h2>Come join us!</h2>
-            <p>
-              We are so excited to have you here. If you haven't already, create an account
-              to get access to exclusive offers, rewards, and discounts.
-            </p>
-            <button id="signin-btn" onClick={() => setIsSignup(false)}>
-              Already have an account? Signin.
-            </button>
-          </div>
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
